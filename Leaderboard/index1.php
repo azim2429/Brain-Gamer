3df3d74c3202b031f382343a5536980616701766
bryanphil <bryanphil31@gmail.com>
2020-09-08 20:35:50 +0530

    leaderboard

---
+++ Leaderboard/index1.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leaderboard</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    
+<div class="leaderboard">
+  <header>
+    <h1>Leader Board</h1>
+    <img src="https://i.imgur.com/xUSrEpd.png" alt="">
+    <nav>
+      <a href="" class="active">Top Scorers</a>
+      <!-- <a href="">Duo</a>
+      <a href=""></a>
+       -->
+    </nav>
+  </header>
+  <table>
+    <thead>
+      <tr>
+        <th class="rank">Rank </th>
+        <th class="name">Username</th>
+        <th class="sp">Score</th>
+        <!-- <th class="kd"></th> -->
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="rank">1</td>
+        <td class="name">Sc0utOP</td>
+        <td class="sp">6308</td>
+        
+      </tr>
+      <tr>
+        <td class="rank">2</td>
+        <td class="name">MortaL</td>
+        <td class="sp">6201</td>
+        
+      </tr>
+      <tr>
+        <td class="rank">4</td>
+        <td class="name">CarryMinati</td>
+        <td class="sp">5308</td>
+        
+      </tr>
+      <tr>
+        <td class="rank">5</td>
+        <td class="name">Bot</td>
+        <td class="sp">5108</td>
+       
+      </tr>
+      <tr>
+        <td class="rank">6</td>
+        <td class="name">Nick</td>
+        <td class="sp">4508</td>
+        
+      </tr>
+      <tr>
+        <td class="rank">7</td>
+        <td class="name">Jamie</td>
+        <td class="sp">4108</td>
+        
+      </tr>
+      <tr>
+        <td class="rank">8</td>
+        <td class="name">Mark</td>
+        <td class="sp">4008</td>
+        
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+</body>
+</html>
